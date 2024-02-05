b2f30bddc7ef94c8fa12017cf15dc9b224058a2e
Linice <Kawinwong_ice@hotmail.com>
2024-02-05 21:47:54 +0700

    -

--- tuab-frontend/tuab-project/src/App.vue
+++ tuab-frontend/tuab-project/src/App.vue
@@ -37,14 +37,14 @@ export default {
 </script>
 
 <style>
-#App {
+/* #App {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 body{
   background: #2c3e50;
 }
