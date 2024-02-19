206c2b73fee1f4a556784615b3212ca53de71ebd
Linice <Kawinwong_ice@hotmail.com>
2024-02-19 21:20:30 +0700

    -

--- tuab-backend/app.js
+++ tuab-backend/app.js
@@ -12,6 +12,7 @@ require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+// var registerRouter = require('./routes/register');
 
 var app = express();
 
@@ -40,6 +41,7 @@ app.use(bodyParser.json());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+// app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
